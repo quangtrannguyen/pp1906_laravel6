b2b5ae76ae5b680a5a478ac7fba30f73beee8d08
quangtrannguyen <quangtrannguyen7@gmail.com>
2019-10-12 02:13:38 +0700

    CustomLayouts

---
+++ resources/views/layouts/includes/header.blade.php
@@ -0,0 +1,55 @@
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Store Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+
+  <!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="theme/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="theme/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="theme/css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="theme/css/magnific-popup.css">
+
+	<!-- Flexslider  -->
+	<link rel="stylesheet" href="theme/css/flexslider.css">
+
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="theme/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="theme/css/owl.theme.default.min.css">
+	
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="theme/css/bootstrap-datepicker.css">
+	<!-- Flaticons  -->
+	<link rel="stylesheet" href="theme/fonts/flaticon/font/flaticon.css">
+
+	<!-- Theme style  -->
+	<link rel="stylesheet" href="theme/css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="theme/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
---
+++ resources/views/layouts/includes/script.blade.php
@@ -0,0 +1,21 @@
+<!-- jQuery -->
+	<script src="theme/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="theme/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="theme/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="theme/js/jquery.waypoints.min.js"></script>
+	<!-- Flexslider -->
+	<script src="theme/js/jquery.flexslider-min.js"></script>
+	<!-- Owl carousel -->
+	<script src="theme/js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="theme/js/jquery.magnific-popup.min.js"></script>
+	<script src="theme/js/magnific-popup-options.js"></script>
+	<!-- Date Picker -->
+	<script src="theme/js/bootstrap-datepicker.js"></script>
+	<!-- Stellar Parallax -->
+	<script src="theme/js/jquery.stellar.min.js"></script>
+	<!-- Main -->
+	<script src="theme/js/main.js"></script>

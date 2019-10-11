9590c77487947b62ea5787058ffc60c2a4211095
quangtrannguyen <quangtrannguyen7@gmail.com>
2019-10-12 01:16:03 +0700

    LayoutsBlade

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,39 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/home', function () {
+    return view('theme.store.index');
+});
+
+Route::get('/shop', function () {
+    return view('theme.store.shop');
+});
+
+Route::get('/blog', function () {
+    return view('theme.store.blog');
+});
+
+Route::get('/about', function () {
+    return view('theme.store.about');
+});
+
+Route::get('/cart', function () {
+    return view('theme.store.cart');
+});
+
+Route::get('/checkout', function () {
+    return view('theme.store.checkout');
+});
+
+Route::get('/product-detail', function () {
+    return view('theme.store.product-detail');
+});
+
+Route::get('/add-to-wishlist', function () {
+    return view('theme.store.add-to-wishlist');
+});
+
+Route::get('/order-complete', function () {
+    return view('theme.store.order-complete');
+});

f5d766d5a91abaad8127a3dc5a2539eedb442220
quangtrannguyen <quangtrannguyen7@gmail.com>
2019-10-24 01:14:08 +0700

    AdminView

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,6 +1,84 @@
 @extends('layouts.store')
 @section('content')
-quang
+<div class="container">
+    @if (session('status'))
+        <div class="alert alert-danger">
+            {{ session('status') }}
+        </div>
+    @endif
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Create product') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('products.store') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="content" class="col-md-4 col-form-label text-md-right">{{ __('Content') }}</label>
+
+                            <div class="col-md-6">
+                                <textarea id="content" type="text" class="form-control @error('content') is-invalid @enderror" name="content" required autocomplete="content">{{ old('content') }}</textarea>
+
+                                @error('content')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="quantity" class="col-md-4 col-form-label text-md-right">{{ __('Quantity') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="quantity" type="text" class="form-control @error('quantity') is-invalid @enderror" name="quantity" value="{{ old('quantity') }}" required autocomplete="new-quantity">
+
+                                @error('quantity')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Price') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="price" type="text" class="form-control" name="price" value="{{ old('price') }}" required autocomplete="new-price">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Create') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
 		<!-- <aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
@@ -472,8 +550,6 @@ quang
 				</div>
 			</div>
 		</div> -->
-
-		babababababa
 		
 @endsection		
 
